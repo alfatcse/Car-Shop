64bbb878d3fd92c0aa6e912e330cfb7115167af4
Alfat <alfat.cse.21@gmail.com>
2022-11-14 21:07:54 +0100

    initail commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+function App() {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+    }
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
