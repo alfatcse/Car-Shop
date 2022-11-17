1ac88a029970384f7f33eb60ff51a1bb7b1596c3
Alfat <alfat.cse.21@gmail.com>
2022-11-17 23:50:41 +0100

    order row display implemnted

---
+++ src/Pages/Orders/OrderRow.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+const OrderRow = ({ order }) => {
+    const { serviceName, price, email, customer, phone, service } = order;
+    const [orderService, setOrderservice] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5006/service/${service}`)
+            .then(res => res.json())
+            .then(data => setOrderservice(data))
+    }, [service])
+    return (
+        <tr>
+            <th>
+                <label>
+                    <button className='btn btn-ghost'>X</button>
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-24 h-24">
+                            {
+                                orderService?.img &&
+                                <img src={orderService.img} alt="Avatar Tailwind CSS Component" />
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{serviceName}</div>
+                        <div className="text-sm opacity-50">{customer}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">${price}</span>
+            </td>
+            <td>Purple</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    );
+};
+
+export default OrderRow;
