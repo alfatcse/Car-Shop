585982c097895a0931be9beeaf8705741bd0827c
Alfat <alfat.cse.21@gmail.com>
2022-11-17 13:02:47 +0100

    log in implemented

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -1,28 +1,31 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../../Firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const createUser = (email, password) => {
-      return createUserWithEmailAndPassword(auth,email,password);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
-    useEffect(()=>{
-       const unsubscribe= onAuthStateChanged(auth,currentuser=>{
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentuser => {
             console.log(currentuser);
             setUser(currentuser);
         })
-        return ()=>{
+        return () => {
             return unsubscribe();
         }
-    },[])
+    }, [])
     const authinfo = {
         user,
         loading,
         createUser,
-
+        login,
     }
     return (
         <AuthContext.Provider value={authinfo}>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 const Login = () => {
-    const handleLogin=event=>{
+    const {login}=useContext(AuthContext);
+    const handleLogin=(event)=>{
         event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password);
+        login(email,password)
+        .then(result=>{
+            const user=result.user;
+            console.log(user);
+        })
+        .then(e=>console.error(e));
     }
     return (
         <div className="hero w-full my-20">
@@ -18,13 +30,13 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
